5e09279ea53937a34f6a0240de153bbb01604ac2
Akettle44 <andrewjkettle@gmail.com>
2019-08-18 22:14:15 -0700

    Still working on output

--- VideoCapture.cpp
+++ VideoCapture.cpp
@@ -9,13 +9,14 @@ int main(int, char**)
 
 	cap.set(CAP_PROP_FRAME_WIDTH, 800);
  	cap.set(CAP_PROP_FRAME_HEIGHT, 600);
- 	cap.set(CAP_PROP_FPS, 30);
+ 	cap.set(CAP_PROP_FPS, 24);
 	//cap.set(CAP_MODE_YUYV);
 	//cap.set(CAP_PROP_FOURCC,( 'H', '2', '6', '4')); 
 //	SetCaptureProperty(cap,cv::CAP_PROP_FOURCC,cv::CV_FOURCC('H', '2', '6', '4'));
 //	cap.set(cv::CAP_PROP_FOURCC, 0x21);
 
 	cap.set(CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+	//cap.set(CAP_PROP_FOURCC, cv::VideoWriter::fourcc('H', '2', '6', '4'));
 
 	if(!cap.isOpened())
 	{
@@ -30,13 +31,19 @@ int main(int, char**)
 
 	int width = cap.get(3);
 	int height = cap.get(4);
+	int framerate = cap.get(5);
+
+
+	Size SizeOfFrame = cv::Size(800,600);	
 
 	std::cout << "This is the width \n" << width;
 	std::cout << "\nThis is the height \n" << height;
+	std::cout << "\nThis is the framerate \n" << framerate;
 	std::cout << "\n";
 	
 	//VideoWriter video("akoutput.mp4",cv::CAP_FFMPEG, 0x21, 30, Size(width, height));	
-	VideoWriter video("akoutput.avi",cv::CAP_FFMPEG, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, Size(width, height), true);	
+	//VideoWriter video("akoutput.avi",cv::CAP_OPENCV_MJPEG, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 24, SizeOfFrame, true);	
+	VideoWriter video("akoutput.avi",cv::CAP_OPENCV_MJPEG, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 24, SizeOfFrame, true);	
 
 	for(;;)
 	{
