bb37c10f70382cffd97aa835896ec409a4a699ba
Andrew Kettle <andrewjkettle@gmail.com>
2019-12-15 21:55:11 +0000

    Temporary save on threading in case I come back to it in the future

--- src/video/VideoCapture.cpp
+++ src/video/VideoCapture.cpp
@@ -1,6 +1,6 @@
 #include "VideoCapture.hpp"
 
-Videocapture openVideo()
+Videocapture Video()
 {
 	VideoCapture cap(0, cv::CAP_V4L2);
 	cap.set(CAP_PROP_FRAME_WIDTH, 800);
@@ -20,34 +20,21 @@ Videocapture openVideo()
 	int width = cap.get(3); //3 is width
 	int height = cap.get(4); //4 is height
 	int framerate = cap.get(5); //5 is framerate
-/*
-	std::cout << "This is the width \n" << width;
-	std::cout << "\nThis is the height \n" << height;
-	std::cout << "\nThis is the framerate \n" << framerate;
-	std::cout << "\n";
-*/
 
-	return cap;
-}
-
-void writeVideo(VideoCapture cap)
-{
 	VideoWriter video("akoutput.avi",cv::CAP_FFMPEG,cv::VideoWriter::fourcc('H', '2', '6', '4'),cap.get(5),Size(800, 600),true);
-	i
-/*
-	for(fcount; fcount < 1560; fcount++)
+
+	for(;;)
 	{
 		Mat frame;
-		cap >> frame;
-		video << frame; //YOU'RE TELLING ME YOU NEED TO WRITE IT TO THE OUTPUT TOO??? --> 
-		//The above line represents three days of my life that I will never get back, YAY
-		if(frame.empty())
-		{
-			break;
-		}
+		cap >> frame; //reading into container
+		video << frame; //outputting to file 
 	}
-
-*/
 	cap.release();
 	video.release();
+}
+
+boolean breakout(boolean status)
+{
+	return status;
+
 }
