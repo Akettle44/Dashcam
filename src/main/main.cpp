7875958126c131ad53c69807fc70a43633f79b78
Andrew Kettle <andrewjkettle@gmail.com>
2019-12-15 21:55:11 +0000

    Temporary save on threading in case I come back to it in the future

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,64 +1,52 @@
 /*
- * Written by Andrew Kettle
- * functional, but could be cleaned up extensively by removing extra filestreams
+ * Written by Andrew Kettle, last update December 14th
 */
+
+using namespace std;
+
 #include "button.hpp"
 #include "VideoCapture.hpp"
-
+#include <thread>
+ 
 int main()
 {		
+	VideoCapture (*fnptr)(){ Video }; //check spelling, function pointer to open_Video
 	char butval[] = "0";
-	bool state =  false; //state of the program
+	bool state =  false; //control flow begins in NOT recording state
 	FILE *button = NULL;
 	FILE *led = NULL;
-	Mat frame;
-	VideoCapture cap;
-
-	init_GPIOs(button, led);
+	init_GPIOs(button, led); //initializes gpio streams and then closes them, watch out for the closure
 	
-	while(1)
+	while(1) //polling with a seperate thread for inputting and writing video
 	{
-		if(state == true)
-		{
-			//currenlty writing video
-			cap >> frame;
-			video << frame;
+		button = fopen("/sys/class/gpio/gpio20/value", "r");
+		fread(butval, 1, sizeof(butval), button);
+		led = fopen("/sys/class/gpio/gpio49/value", "w");
 
-			if((strcmp(butval, "1") == 10) && (state == true)) 
-			{
-				//stop recording video
-				state = false;
-				fwrite("0", 1, sizeof("0"), led);
-				break; //breaks from infinite while			
-			}
+		if((strcmp(butval, "1") == 10) && (state == false)) 
+		{
+			//start new thread for recording and writing video
+			thread vid(fnptr); //starts new thread of video recording
+			state = true;
+			fwrite("1", 1, sizeof("1"), led);			
+		}
+		else if((strcmp(butval, "1") == 10) && (state == true)) 
+		{
+			//interrupt thread 
+			//stop recording video
+			state = false;
+			fwrite("0", 1, sizeof("0"), led);
+			break; //breaks from infinite while			
 		}
 
-		else
+		if(button != NULL)
 		{
-			button = fopen("/sys/class/gpio/gpio20/value", "r");
-			fread(butval, 1, sizeof(butval), button);
-			led = fopen("/sys/class/gpio/gpio49/value", "w");
-
-			if((strcmp(butval, "1") == 10) && (state == false)) 
-			{
-				//begin recording video
-				cap = openVideo();	
-				state = true;
-				fwrite("1", 1, sizeof("1"), led);			
-			}
-			else if((strcmp(butval, "1") == 10) && (state == true)) 
-			{
-				//stop recording video
-				state = false;
-				fwrite("0", 1, sizeof("0"), led);
-				break; //breaks from infinite while			
-			}
-
-		fclose(button);
-		fclose(led);
-
+			fclose(buton);
 		}
-
+		if(led != NULL)
+		{
+			fclose(led);
+		}
 	}
 
 	if(button != NULL)
