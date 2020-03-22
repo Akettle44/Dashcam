67761835e63837a1b3f314da4bfc9d9dcc8b0120
Andrew Kettle <andrewjkettle@gmail.com>
2020-03-22 16:32:40 +0000

    using pthread, going to try and use std::thread instead

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #include "../button/button.hpp"
 #include "../video/VideoCapture.hpp"
  
-int checkState(bool currstate, FILE *button, FILE *led);
+void* addFrame();
 
 int main()
 {		
@@ -21,59 +21,42 @@ int main()
 	FILE *button = NULL;
 	FILE *led = NULL;
 	int x = -1;
+	int fcount = 0;
+	char butval[] = "0";
 	Mat frame;
 	VideoCapture cap;
 	VideoWriter video;
 	init_GPIOs(button, led); //initializes gpio streams and then closes them
 
-/*	pthread_t check;
-	pthread_create(&check, NULL, checkState, NULL);
-*/
+	pthread_t frames; //declares thread
 	
 	while(1) //polling
 	{
-		x = checkState(state, button, led);
-		cout << "This is x: " << "\n";
-		if(x != -1) //state needs to be changed
+		button = fopen("/sys/class/gpio/gpio20/value", "r");
+		fread(butval, 1, sizeof(butval), button);
+		led = fopen("/sys/class/gpio/gpio49/value", "w"); 
+
+		if((strcmp(butval, "1") == 10) && (state == false)) 
 		{
-			led = fopen("/sys/class/gpio/gpio49/value", "w");
-			if(x == 1) 
-			{
-				//begin recording video
-				cap = initCapture();
-				video = initWriter(cap);
-				state = true;
-				fwrite("1", 1, sizeof("1"), led);
-				usleep(2000); //prevents the on sequence from hanging onto the off sequence			
-			}
-			else if(x == 2) 
-			{
-				//close video
-//				pthread_join(check, NULL);
-//				pthread_exit(NULL);
-				state = false;
-				closeVideo(cap, video);
-				fwrite("0", 1, sizeof("0"), led);
-				cout << "Got to break \n";
-				break; //breaks from infinite while
-			}
-			else
-			{
-				cout << "An error occured" << "\n";
-			}
-			
-		}	
-		else
-		{
-			if(state == true)
-			{
-				cap.set(CAP_PROP_FPS, cap.get(5)); //frame rate keeps hanging after a few seconds, forcing it to 24
-				cout.flush();
-				cout << "Adding frame \n";
-				cap >> frame;
-				video << frame;	
-			}
-		 }
+			//begin recording video
+			cap = initCapture();
+			video = initWriter(cap);
+			pthread_create(&frames, NULL, addFrame, void *(&cap), void *(&video)); //spawns thread
+			state = true;
+			fwrite("1", 1, sizeof("1"), led);
+			usleep(2000); //prevents the on sequence from hanging onto the off sequence			
+		}
+		else if((strcmp(butval, "1") == 10) && (state == true)) 
+		{ 
+			//close video
+			pthread_join(check, NULL);
+			pthread_exit(NULL);
+			state = false;
+			closeVideo(cap, video);
+			fwrite("0", 1, sizeof("0"), led);
+			cout << "Got to break \n";
+			break; //breaks from infinite while
+		}
 	}
 
 	cout << "Everything yeeted \n";
@@ -90,31 +73,14 @@ int main()
 	return 0;
 }
 
-int checkState(bool currstate, FILE *button, FILE *led) //returns 1 if the camera needs to be turned off, 2 if it needs to be turned on, -1 otherwise
+void* addFrame(void *)
 {
-		int rval = 0;
-		char butval[] = "0";
-		button = fopen("/sys/class/gpio/gpio20/value", "r");
-		fread(butval, 1, sizeof(butval), button);
-		led = fopen("/sys/class/gpio/gpio49/value", "w");
-
-		if((strcmp(butval, "1") == 10) && (currstate == false)) 
-		{
-			rval = 1;
-		}
-		else if((strcmp(butval, "1") == 10) && (currstate == true)) 
-		{
-			rval = 2;
-		}
-
-		if(button != NULL)
-		{
-			fclose(button);
-		}
-		if(led != NULL)
-		{
-			fclose(led);
-		}
-
-		return rval;
+	cap.set(CAP_PROP_FPS, cap.get(5)); //frame rate keeps hanging after a few seconds, forcing it to 24
+	cout.flush();
+	cout << "Adding frame ";
+	cap >> frame;
+	video << frame;	
+	fcount++;	
+	cout << fcount << "\n";
 }
+
